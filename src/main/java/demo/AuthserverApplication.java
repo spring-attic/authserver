610c4c343ca0ddf5f4a0aa8f5dfbaefa9d15b7c0
Dave Syer <dsyer@pivotal.io>
2014-11-21 11:55:25 +0000

    Convert to JWT tokens

--- src/main/java/demo/AuthserverApplication.java
+++ src/main/java/demo/AuthserverApplication.java
@@ -1,24 +1,30 @@
 package demo;
 
+import java.security.KeyPair;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class AuthserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(AuthserverApplication.class, args);
 	}
 
 	@Configuration
@@ -27,6 +33,16 @@ public class Application {
 
 		@Autowired
 		private AuthenticationManager authenticationManager;
+
+		@Bean
+		public JwtAccessTokenConverter jwtAccessTokenConverter() {
+			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+			KeyPair keyPair = new KeyStoreKeyFactory(
+					new ClassPathResource("keystore.jks"), "foobar".toCharArray())
+					.getKeyPair("test");
+			converter.setKeyPair(keyPair);
+			return converter;
+		}
 
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -40,13 +56,13 @@ public class Application {
 		@Override
 		public void configure(AuthorizationServerEndpointsConfigurer endpoints)
 				throws Exception {
-			endpoints.authenticationManager(authenticationManager);
+			endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
 		}
 
 		@Override
 		public void configure(AuthorizationServerSecurityConfigurer oauthServer)
 				throws Exception {
-			oauthServer.checkTokenAccess("permitAll()");
+			oauthServer.tokenKeyAccess("permitAll()");
 		}
 
 	}
